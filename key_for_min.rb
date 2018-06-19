16986b063d00af298b5d12d9080b0da48731a2f6
Yicheng Jin <bobjin42@gmail.com>
2018-06-19 16:09:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash = {:yicheng => 30, :mike => 35, :annie => 40}
+  return key_for_min_value(name_hash)
 end
